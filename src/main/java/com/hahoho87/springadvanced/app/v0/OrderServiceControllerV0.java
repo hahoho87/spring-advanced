8ab423255a3f42b16c960edcffc6a3535056587d
hahoho87 <hahoho87@gmail.com>
2021-11-03 23:47:49 +0900

    add test api

---
+++ src/main/java/com/hahoho87/springadvanced/app/v0/OrderServiceControllerV0.java
@@ -0,0 +1,23 @@
+package com.hahoho87.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderServiceControllerV0 {
+
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
