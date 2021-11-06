78e932ab6267d98a886c32f82f88929df912a1ee
hahoho87 <bbaing4564@gmail.com>
2021-11-07 00:33:16 +0900

    로그 추적기 v2 적용

---
+++ src/main/java/com/hahoho87/springadvanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,36 @@
+package com.hahoho87.springadvanced.app.v2;
+
+import com.hahoho87.springadvanced.app.trace.TraceId;
+import com.hahoho87.springadvanced.app.trace.TraceStatus;
+import com.hahoho87.springadvanced.app.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+
+    private final HelloTraceV2 trace;
+
+    public void save(TraceId traceId, String itemId) {
+        sleep(1000);
+        TraceStatus status = trace.beginSync(traceId, "OrderRepository.save()");
+        try {
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/hahoho87/springadvanced/app/v2/OrderServiceControllerV2.java
@@ -0,0 +1,28 @@
+package com.hahoho87.springadvanced.app.v2;
+
+import com.hahoho87.springadvanced.app.trace.TraceStatus;
+import com.hahoho87.springadvanced.app.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderServiceControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = trace.begin("OrderController.request()");
+        try {
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+        return "ok";
+    }
+}
