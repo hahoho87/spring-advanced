8b04e420e30a5c371e6886f3b0120987e0fef2cc
bky4564 <bky4564@hunet.co.kr>
2021-11-12 12:34:01 +0900

    기존 로직에 템플릿 메소드 패턴 적용

---
+++ src/main/java/com/hahoho87/springadvanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,36 @@
+package com.hahoho87.springadvanced.app.v4;
+
+import com.hahoho87.springadvanced.trace.logtrace.LogTrace;
+import com.hahoho87.springadvanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.excute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/hahoho87/springadvanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,28 @@
+package com.hahoho87.springadvanced.trace.template;
+
+import com.hahoho87.springadvanced.trace.TraceStatus;
+import com.hahoho87.springadvanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T excute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+            T result = call();
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
