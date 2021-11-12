465df0f5b8d869583edbbbea0adb57d46767e9cf
bky4564 <bky4564@hunet.co.kr>
2021-11-12 14:08:41 +0900

    전략 패턴 예제생성

---
+++ src/test/java/com/hahoho87/springadvanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,44 @@
+package com.hahoho87.springadvanced.trace.strategy;
+
+import com.hahoho87.springadvanced.trace.strategy.code.strategy.ContextV1;
+import com.hahoho87.springadvanced.trace.strategy.code.strategy.StrategyLogic1;
+import com.hahoho87.springadvanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyPatternTest() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        log.info("비지니스 로직 1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        log.info("비지니스 로직 2 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    @Test
+    void strategyPatternTestV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+}
---
+++ src/test/java/com/hahoho87/springadvanced/trace/strategy/code/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package com.hahoho87.springadvanced.trace.strategy.code.strategy;
+
+public interface Strategy {
+    void call();
+}
