4915cdb99b7e63d373c962bebea9e942d5a41465
bky4564 <bky4564@hunet.co.kr>
2021-11-12 09:14:42 +0900

    템플릿 메서드 패턴 생성 V1

--- src/test/java/com/hahoho87/springadvanced/trace/template/TemplateMethodTest.java
+++ src/test/java/com/hahoho87/springadvanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package com.hahoho87.springadvanced.trace.template;
 
+import com.hahoho87.springadvanced.trace.template.code.AbstractTemplate;
+import com.hahoho87.springadvanced.trace.template.code.SubClassLogic1;
+import com.hahoho87.springadvanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +29,13 @@ class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
+    }
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.excute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.excute();
     }
 }
