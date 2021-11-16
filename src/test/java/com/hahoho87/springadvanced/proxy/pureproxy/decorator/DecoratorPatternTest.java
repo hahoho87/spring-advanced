64b51c9a3f04f92a820c03af625220fe421bd360
bky4564 <bky4564@hunet.co.kr>
2021-11-16 14:13:16 +0900

    decorator 패턴 chaining 적용 test 생성

--- src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -3,6 +3,7 @@ package com.hahoho87.springadvanced.proxy.pureproxy.decorator;
 import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.DecoratorClientClient;
 import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.MessageDecorator;
 import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.RealComponent;
+import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,15 @@ class DecoratorPatternTest {
         RealComponent realComponent = new RealComponent();
         MessageDecorator messageDecorator = new MessageDecorator(realComponent);
         DecoratorClientClient client = new DecoratorClientClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decoratorTest2() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorClientClient client = new DecoratorClientClient(timeDecorator);
         client.execute();
     }
 }
---
+++ src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -0,0 +1,25 @@
+package com.hahoho87.springadvanced.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeDecorator implements Component {
+
+    private final Component component;
+
+    public TimeDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+        String result = component.operation();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("Time Decorator 종료 resultTime = {}ms", resultTime);
+
+        return result;
+    }
+}
