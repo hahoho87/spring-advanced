ffe6dd450bcfe0c170a46c057e99fca17b329907
bky4564 <bky4564@hunet.co.kr>
2021-11-16 12:50:58 +0900

    proxy 패턴 적용 test 생성

--- src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package com.hahoho87.springadvanced.proxy.pureproxy.proxy;
 
+import com.hahoho87.springadvanced.proxy.pureproxy.proxy.code.CacheProxy;
 import com.hahoho87.springadvanced.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import com.hahoho87.springadvanced.proxy.pureproxy.proxy.code.RealSubject;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,16 @@ class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
