149ecff07e3878c604bdeb01467f23ec530baa8d
bky4564 <bky4564@hunet.co.kr>
2021-11-16 13:42:06 +0900

    decorator 패턴 적용 test 생성

--- src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/com/hahoho87/springadvanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,7 @@
 package com.hahoho87.springadvanced.proxy.pureproxy.decorator;
 
 import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.DecoratorClientClient;
+import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.MessageDecorator;
 import com.hahoho87.springadvanced.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,14 @@ class DecoratorPatternTest {
     void noDecorator() {
         RealComponent realComponent = new RealComponent();
         DecoratorClientClient client = new DecoratorClientClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decoratorTest1() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorClientClient client = new DecoratorClientClient(messageDecorator);
         client.execute();
     }
 }
