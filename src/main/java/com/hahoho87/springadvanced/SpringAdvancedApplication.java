32664a77002b9ff8bc38abca2df3c1f37bc774dc
bky4564 <bky4564@hunet.co.kr>
2021-11-18 12:47:12 +0900

    jdk dynamic 프록시 적용

--- src/main/java/com/hahoho87/springadvanced/SpringAdvancedApplication.java
+++ src/main/java/com/hahoho87/springadvanced/SpringAdvancedApplication.java
@@ -1,6 +1,6 @@
 package com.hahoho87.springadvanced;
 
-import com.hahoho87.springadvanced.proxy.config.v1_proxy.concrete_proxy.ConcreteProxyConfig;
+import com.hahoho87.springadvanced.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.hahoho87.springadvanced.trace.logtrace.LogTrace;
 import com.hahoho87.springadvanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "com.hahoho87.springadvanced.proxy.app")
 public class SpringAdvancedApplication {
 
---
+++ src/main/java/com/hahoho87/springadvanced/proxy/config/v2_dynamicproxy/handler/LogTraceBasicHandler.java
@@ -0,0 +1,34 @@
+package com.hahoho87.springadvanced.proxy.config.v2_dynamicproxy.handler;
+
+import com.hahoho87.springadvanced.trace.TraceStatus;
+import com.hahoho87.springadvanced.trace.logtrace.LogTrace;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogTraceBasicHandler implements InvocationHandler {
+
+    private final Object target;
+    private final LogTrace logTrace;
+
+    public LogTraceBasicHandler(Object target, LogTrace logTrace) {
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        TraceStatus status = null;
+
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = logTrace.begin(message);
+            Object result = method.invoke(target, args);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
